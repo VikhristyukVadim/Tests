0ae2ed693cdcce3d2b41d900e419eec83df0353f
VikhristyukVadim <91058237+VikhristyukVadim@users.noreply.github.com>
2021-09-22 11:38:56 +0300

    Update server_app.py

--- server_app.py
+++ server_app.py
@@ -1,88 +1,44 @@
-# import json
-from flask import Flask, jsonify, request
-from pony_file import db, insert_notes
-from pony.orm import db_session
+from flask import Flask
+from pony_file import insert_notes, get_notes_list, change_note, del_note
 
 app = Flask(__name__)
 
 client = app.test_client()
 
-json_example = [
-    {
-        "id": 1,
-        "quote": "The business plans of the next 10,000 years"
-    },
-    {
-        "id": 2,
-        "quote": "Last"
-    }
-]
-
 
 @app.route("/example", methods=['GET'])
 def get_list():
-    print("==============", db.Notes)
-    # return jsonify(json_example)
-
-    result = {
-        'data': []
-    }
-    with db_session:
-        res = db.select(n for n in db.Notes)
-        for i in res:
-            result['data'].append({'quote': i.quote})
+    result = get_notes_list()
+    print('NOTES_LIST: ', result['data'])
     return result, 200
 
 
 @app.route("/example", methods=['POST'])
-def update_list(message):
-    # new_message = message
+def add_item(message):
     insert_notes(message)
-    # new_one = request.json
-    # json_example.append(new_one)
-    # return jsonify(json_example)
+    print('Your new note is added successfully: ', message)
     return message, 200
 
 
-@app.route("/example/<int:json_example_id>", methods=['PUT'])
-def update_example(json_example_id):
-    item = next((x for x in json_example if x['id'] == json_example_id), None)
-    params = request.json
-    if not item:
-        return {'message': 'no examples with this id'}, 400
-    item.update(params)
-    return item
+@app.route("/example", methods=['PUT'])
+def change_note_by_id(new_txt):
+    data = change_note(new_txt)
+    print('You have changed the note.')
+    return data, 200
 
 
-@app.route("/example/<int:json_example_id>", methods=['DELETE'])
-def delete_example(json_example_id):
-    idx, _ = next((x for x in enumerate(json_example) if x[1]['id'] == json_example_id), (None, None))
-    json_example.pop(idx)
-    return '', 204
+@app.route("/example", methods=['PUT'])
+def find_note_by_txt(word):
+    data = change_note(word)
+    print('You have find note num', data)
+    return data, 200
+
+
+@app.route("/example", methods=['DELETE'])
+def delete_note(note_id):
+    del_note(note_id)
+    return 'Object vs id = {note_id} deleted', 204
 
 
 if __name__ == "__main__":
     app.run()
-# @app.route('/hello')
-# def hello():
-#     return '<div>Hello, World</div>'
-
-# from markupsafe import escape
-#
-#
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     # show the user profile for that user
-#     return f'User {escape(username)}'
-#
-#
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return f'Post {post_id}'
-#
-#
-# @app.route('/path/<path:subpath>')
-# def show_subpath(subpath):
-#     # show the subpath after /path/
-#     return f'Subpath {escape(subpath)}'
